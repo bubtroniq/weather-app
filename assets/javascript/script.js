dfca4f730656e89509a84d48d87bdfb3be6e8576
bubtroniq <joitescurazvan@gmail.com>
2023-03-08 07:06:14 +0000

    Add search and setbackground functions in script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,10 @@
-let city = document.querySelector('input').value;
-
 let weather = {
     city: 'tokyo',
     apiKey: '6ed13b8704e280c7b07c7f3594d5ffc1',
     fetchWeather: function(city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city
         +"&units=metric&APPID=" 
-        + this.apiKey)
+        + weather.apiKey)
         .then((response) => response.json())
         .then((data) => this.displayWeather(data))
     },
@@ -19,14 +17,23 @@ let weather = {
         const {speed} = data.wind;
         console.log(name, icon, description, temp, humidity, speed);
         document.querySelector('.city').innerText = "Weather in " + name;
+        document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon +'@2x.png';
         document.querySelector('.description').textContent = description;
         document.querySelector('.temp').innerHTML = `Temperature:  + ${temp}<span>&#8451;</span>`;
         document.querySelector('.humidity').innerHTML = `Humidity: ${humidity}<span>&#37;</span>`;
-        document.querySelector('.speed').textContent = `Speed: ${speed} KM/H`;
-        
-    }
+        document.querySelector('.speed').textContent = `Speed: ${speed} KM/H`;   
+    },
 
+    search: function() {
+        weather.fetchWeather(document.querySelector('.search-bar').value);
+    },
+
+    setBackground: function() {
+            document.querySelector('.container').style.backgroundImage = "url('../assets/images/snow.jpg')";
+    }
 }
 
-document.querySelector('button').addEventListener('click', weather.fetchWeather);
- 
+document.querySelector('button').addEventListener('click', function() {
+    weather.search();
+    weather.setBackground();
+});
