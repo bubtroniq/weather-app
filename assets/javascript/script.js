741759b882f159519f65f71ae42354cc27c3ad95
bubtroniq <joitescurazvan@gmail.com>
2023-03-08 12:03:06 +0000

    Fix setBackground function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,7 +18,7 @@ let weather = {
         console.log(name, icon, description, temp, humidity, speed);
         document.querySelector('.city').innerText = "Weather in " + name;
         document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon +'@2x.png';
-        document.querySelector('.description').textContent = description;
+        document.querySelector('.description').innerText = description;
         document.querySelector('.temp').innerHTML = `Temperature:  + ${temp}<span>&#8451;</span>`;
         document.querySelector('.humidity').innerHTML = `Humidity: ${humidity}<span>&#37;</span>`;
         document.querySelector('.speed').textContent = `Speed: ${speed} KM/H`;   
@@ -28,9 +28,31 @@ let weather = {
         weather.fetchWeather(document.querySelector('.search-bar').value);
     },
 
+    
+
     setBackground: function() {
-            document.querySelector('.container').style.backgroundImage = "url('../assets/images/snow.jpg')";
+       let containerBg = document.querySelector(".container");
+       switch(document.querySelector('.description').innerText) {
+            case "light rain":
+                containerBg.style.backgroundImage = "url('assets/images/light-rain.jpg')";
+                break;
+            case "snow":
+                containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
+                break;
+            case "overcast clouds":
+                containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
+                break;
+            default:
+                containerBg.style.backgroundImage = "url('assets/images/sunny.jpg')"
+        }
     }
+
+    // setBackground: function() {
+    //     let containerBg = document.querySelector(".container");
+    //     if(weather.description === "light rain") {
+    //         containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
+    //     }
+    // }
 }
 
 document.querySelector('button').addEventListener('click', function() {
