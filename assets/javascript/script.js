38d7bde7b0c7eaaa69892c17be3aeb683983c97e
bubtroniq <joitescurazvan@gmail.com>
2023-03-12 14:30:51 +0000

    Add google maps structure and js code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,22 +1,4 @@
-// Google maps
 
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 5,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-}
-  
-  window.initMap = initMap;
 
 // let q = document.querySelector('.search-bar').value;
 
@@ -71,10 +53,6 @@ const displayDetailedWeather = (data2) => {
     console.log(country);
 };
 
-
-
-
-fetchDetailedWeather();
 const fetchWeather = (city) => {
 let apiKey = '6ed13b8704e280c7b07c7f3594d5ffc1';
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city
@@ -84,15 +62,7 @@ let apiKey = '6ed13b8704e280c7b07c7f3594d5ffc1';
         .then((data) => displayWeather(data))
 };
 
-// const displayCountry = (data) => {
-//     const {country} = data.sys;
-//     console.log(country);
-//     document.querySelector('.country').innerText = `Country: ${country}`;
-// };
-// displayCountry();
-
-
- const displayWeather = (data) => {
+ const displayWeather = (data,) => {
         const {country} = data.sys;
         const {name} = data;
         const {icon, description} = data.weather[0];
@@ -100,7 +70,7 @@ let apiKey = '6ed13b8704e280c7b07c7f3594d5ffc1';
         const {speed} = data.wind;
         const {lat} = data.coord;
         const {lon} = data.coord;
-        // console.log(name, icon, description, temp, humidity, speed);
+        console.log(name, icon, description, temp, humidity, speed, lat, lon);
         document.querySelector('.city').innerText = "Weather in " + name;
         document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon +'@2x.png';
         document.querySelector('.description').innerText = description;
@@ -108,8 +78,10 @@ let apiKey = '6ed13b8704e280c7b07c7f3594d5ffc1';
         document.querySelector('.humidity').innerHTML = `Humidity: ${humidity}<span>&#37;</span>`;
         document.querySelector('.speed').textContent = `Wind Speed: ${speed} KM/H`;   
         document.querySelector('.country').innerText = `Country: ${country}`;
-        document.querySelector('.coord').textContent = `Coordinates: Lat:${lat} + Lon:${lon};`
-};      
+        document.querySelector('.coord').textContent = `Coordinates: Lat :${lat}, Lon: ${lon}`;
+
+};   
+         
 
 const setBackground = () => {
         let containerBg = document.querySelector(".container");
@@ -137,12 +109,6 @@ const search = () => {
         fetchWeather(document.querySelector('.search-bar').value);
         fetchDetailedWeather(document.querySelector('.search-bar').value);
 };
-    // setBackground: function() {
-    //     let containerBg = document.querySelector(".container");
-    //     if(weather.description === "light rain") {
-    //         containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
-    //     }
-    // }
 
 
 document.querySelector('button').addEventListener('click', () => {
@@ -150,53 +116,23 @@ document.querySelector('button').addEventListener('click', () => {
     search();
     
 });
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    // let coords = (data) => {
+    //     let {lat, lon} = data.coord;
+    //     console.log(lat, lon);
+        const uluru = { lat: -25.344, lng: 131.031 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
 
-// const options = {
-// 	method: 'GET',
-//     type: 'CITY',
-// 	headers: {
-// 		'X-RapidAPI-Key': '11c2ba5c89msh04e9cbea46bbbadp16ac1ajsnf9171e5dbb5b',
-// 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-// 	}
-// };
-
-// fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities', options)
-// 	.then(response => response.json())
-// 	.then(response => console.log(response))
-// 	.catch(err => console.error(err));
-
-    // var city = 'San Francisco'
-    // $.ajax({
-    //     method: 'GET',
-    //     url: 'https://api.api-ninjas.com/v1/city?name=' + city,
-    //     headers: { 'X-Api-Key': 'YOUR_API_KEY'},
-    //     contentType: 'application/json',
-    //     success: function(result) {
-    //         console.log(result);
-    //     },
-    //     error: function ajaxError(jqXHR) {
-    //         console.error('Error: ', jqXHR.responseText);
-    //     }
-    // });
-
-// // Initialize and add the map
-// function initMap() {
-//     // The location of Uluru
-//     const uluru = { lat: -25.344, lng: 131.031 };
-//     // The map, centered at Uluru
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//       zoom: 4,
-//       center: uluru,
-//     });
-//     // The marker, positioned at Uluru
-//     const marker = new google.maps.Marker({
-//       position: uluru,
-//       map: map,
-//     });
-//   }
-  
-//   window.initMap = initMap;
-
-    
-
-
+  window.initMap = initMap;
