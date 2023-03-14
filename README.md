f3222f05c30046dfaa009030860e4d30a38f62c7
bubtroniq <joitescurazvan@gmail.com>
2023-03-14 23:45:35 +0000

    Add comments in index.html setBackground in script.js update README

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # Weather App
 
+Weathe App is a simple website with modern and responsive design where the user can find geolocation information like temperature, wind speed, humidity, location displayed on a map for better orientation, all this in real time.
+
+
+
+Weather App is intended for any kind of user who wants to find weather and location information for a specific location using the search input and the search button at the top.
+The App its very easy to use with a simple and sleek design.
+
 
 
 ## UX
 
-
+A simple, yet modern design, wich contains an input element and a search button in the first phase.
+After inserting a location in the text input and click search, two more divs will be added. First div contains weather data, second div contains a map with the location pinned.
 
 ### Colour Scheme
 
@@ -55,6 +63,8 @@
 
 - Alot of trial and error fetching both API's and displaying data on the go modifying DOM structure.
 - Had to setTimeout() on the changeBackground function to fix background picture in one click, otherwise 2 click requierd in order to set the proper background definded in switch statement.
+- Lots of issues with retrievieng coordinates from data object
+- After first search map is not updating with new coordinates
 
 
 
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,42 +3,83 @@
 let city = document.getElementsByTagName('input').innerText;
 const fetchWeather = (city) => {
     const apiKey = '6ed13b8704e280c7b07c7f3594d5ffc1';
-            fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city
-            +"&units=metric&APPID=" 
-            + apiKey)
-            .then((response) => response.json())
-            .then((data) => showWeather(data));
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city
+        + "&units=metric&APPID="
+        + apiKey)
+        .then((response) => response.json())
+        .then((data) => showWeather(data));
 
 };
 
 // Function to display weather on the page 
 const showWeather = (data) => {
-    const {lat: latitude, lon: longitude} = data.coord;
-    const {country} = data.sys;
-    const {name} = data;
-    const {icon, description} = data.weather[0];
-    const {temp, humidity, feels_like} = data.main;
-    const {speed} = data.wind;
+    const { lat: latitude, lon: longitude } = data.coord;
+    const { country } = data.sys;
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity, feels_like } = data.main;
+    const { speed } = data.wind;
 
 
-    console.log(data ,name, icon, description, temp, humidity, speed, latitude, longitude);
+ 
+
+
+
+    // console.log(data, name, icon, description, temp, humidity, speed, latitude, longitude);
     document.querySelector('.city').innerText = "Weather in " + name;
-        
-    document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + icon +'@2x.png';
-    
+
+    document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+
     document.querySelector('.description').innerText = description;
-    
+
     document.querySelector('.temp').innerHTML = `Temperature:  + ${temp}<span> &#8451;</span>`;
-    
+
     document.querySelector('.feels').innerHTML = `Feels like: ${feels_like}<span> &#8451;</span>`
-    
+
     document.querySelector('.humidity').innerHTML = `Humidity: ${humidity}<span>&#37;</span>`;
-    
-    document.querySelector('.speed').textContent = `Wind Speed: ${speed} KM/H`;   
-    
+
+    document.querySelector('.speed').textContent = `Wind Speed: ${speed} km/h`;
+
     document.querySelector('.country').innerText = `Country: ${country}`;
-        
+
     document.querySelector('.coord').textContent = `Coordinates: Lat :${latitude} °, Lon: ${longitude} °`;
+
+    // Function for changing background image regarding description value
+
+    const setBackground = () => {
+        let containerBg = document.querySelector("body");
+        // console.log(description);
+        switch(description) {
+            case "light rain":
+                 containerBg.style.backgroundImage = "url('assets/images/light-rain.jpg')";
+                 break;
+            case "snow":
+                 containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
+                 break;
+            case "light snow":
+                    containerBg.style.backgroundImage = "url('assets/images/light-snow.jpg')";
+                    break;
+            case "light intensity drizzle":
+                 containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
+                 break;
+            case "broken clouds":
+                 containerBg.style.backgroundImage = "url('assets/images/broken-clouds.jpg')";
+                 break;
+            case "scattered clouds":
+                containerBg.style.backgroundImage = "url('assets/images/scattered-clouds.jpg')";
+                break;
+            default:
+                 containerBg.style.backgroundImage = "url('assets/images/sunny.jpg')"
+         }
+        //  setTimeout(containerBg.setBackground, 2000);
+         
+    };
+
+    setBackground();
+
+
+
+
 
 
     //Displaying the map
@@ -48,16 +89,14 @@ const showWeather = (data) => {
     const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
     const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-    const tiles = L.tileLayer(tileUrl, {attribution});
+    const tiles = L.tileLayer(tileUrl, { attribution });
     tiles.addTo(mymap);
 
 
 
     L.marker([latitude, longitude]).addTo(mymap);
-    mymap.setView([latitude, longitude], 11);
+    mymap.setView([latitude, longitude], 13);
     marker.setLatLng([latitude, longitude]);
-
-   
 }
 
 // Fetch weather callback function
@@ -67,10 +106,13 @@ const search = () => {
 };
 
 
+
+
 // Event listener to display weather and location on map clicking search button
 document.querySelector('button').addEventListener('click', () => {
     search();
     showWeather();
+    setBackground()
 });
 
 
@@ -78,7 +120,7 @@ document.querySelector('button').addEventListener('click', () => {
 
 let input = document.querySelector('input');
 input.addEventListener('keypress', (event) => {
-    if(event.key === "Enter") {
+    if (event.key === "Enter") {
         document.querySelector('button').click();
     }
 })
