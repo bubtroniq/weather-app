04eea2acd2a19b419687d965217c8d813232203f
bubtroniq <joitescurazvan@gmail.com>
2023-03-22 00:23:43 +0000

    Add comments to code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,33 +1,31 @@
 //Fetching Data from weatherAPI
-
-
-
 const fetchWeather = (city) => {
     const apiKey = '6ed13b8704e280c7b07c7f3594d5ffc1';
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city+ "&units=metric&APPID=" + apiKey)
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&APPID=" + apiKey)
         .then((response) => {
-            if(!response.ok) {
+            //Conditionals cheking for good/bad response
+            if (!response.ok) {
                 return response.text().then(text => document.querySelector('.error').innerText = 'Cant find your city!')
-               } else {
-               return response.json();
-             }    
-            })
+            } else {
+                return response.json();
+            }
+        })
         .then((data) => showWeather(data))
-        //catch an error here
+        //catch errors from bad user input
         .catch((error) => {
-           console.log('ERROR is: ', error);        
-})};
+            console.log('ERROR is: ', error);
+        })
+};
 
 // Fetch weather callback function
-
 const search = () => {
     let city = document.querySelector('input').value;
-        fetchWeather(city);
-   
-    //catch another error here
-    try{
-    showWeather();
-    } catch(error) {
+    fetchWeather(city);
+
+    //catch errors from bad user input
+    try {
+        showWeather();
+    } catch (error) {
         console.log('THIS ERROR is: ', error);
     }
 };
@@ -39,18 +37,17 @@ const search = () => {
 document.querySelector('button').addEventListener('click', () => {
     let city = document.querySelector('input').value;
     //Conditionals for user feedback 
-    if(city === '') {
+    if (city === '') {
         document.querySelector('.error').innerText = 'Enter a location';
-    } else if(city !== '') {
+    } else if (city !== '') {
         document.querySelector('.error').innerText = '';
         search();
     }
-    
+
 });
 
 
 //Event listener to search on pressing Enter key
-
 let input = document.querySelector('input');
 input.addEventListener('keypress', (event) => {
     if (event.key === "Enter") {
@@ -66,7 +63,7 @@ const showWeather = (data) => {
     const { icon, description } = data.weather[0];
     const { temp, humidity, feels_like } = data.main;
     const { speed } = data.wind;
-    // console.log(data, name, icon, description, temp, humidity, speed, latitude, longitude);
+
     document.querySelector('.city').innerText = "Weather in " + name;
 
     document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
@@ -85,35 +82,36 @@ const showWeather = (data) => {
 
     document.querySelector('.coord').textContent = `Coordinates: Lat :${latitude} °, Lon: ${longitude} °`;
 
-    // Function for changing background image regarding description value
 
+    // Function for changing background image regarding description value
     const setBackground = () => {
         let containerBg = document.querySelector("body");
         // console.log(description);
-        switch(description) {
+        switch (description) {
             case "light rain":
-                 containerBg.style.backgroundImage = "url('assets/images/light-rain.jpg')";
-                 break;
+                containerBg.style.backgroundImage = "url('assets/images/light-rain.jpg')";
+                break;
             case "snow":
-                 containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
-                 break;
+                containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
+                break;
             case "light snow":
-                    containerBg.style.backgroundImage = "url('assets/images/light-snow.jpg')";
-                    break;
+                containerBg.style.backgroundImage = "url('assets/images/light-snow.jpg')";
+                break;
             case "light intensity drizzle":
-                 containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
-                 break;
+                containerBg.style.backgroundImage = "url('assets/images/snow.jpg')";
+                break;
             case "broken clouds":
-                 containerBg.style.backgroundImage = "url('assets/images/broken-clouds.jpg')";
-                 break;
+                containerBg.style.backgroundImage = "url('assets/images/broken-clouds.jpg')";
+                break;
             case "scattered clouds":
                 containerBg.style.backgroundImage = "url('assets/images/scattered-clouds.jpg')";
                 break;
             default:
-                 containerBg.style.backgroundImage = "url('assets/images/sunny.jpg')"
-         }
-         
+                containerBg.style.backgroundImage = "url('assets/images/sunny.jpg')"
+        }
+
     };
+    //Calling setBackground function
     setBackground();
 
     //Displaying the map
@@ -125,7 +123,7 @@ const showWeather = (data) => {
     const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
     const tiles = L.tileLayer(tileUrl, { attribution });
     tiles.addTo(mymap);
-
+    //Add map marker at specified location
     L.marker([latitude, longitude]).addTo(mymap);
     mymap.setView([latitude, longitude], 13);
     marker.setLatLng([latitude, longitude]);
